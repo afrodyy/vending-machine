64bddc1553b176c391e2156434ba3cb10bcbe3ce
Herdian Afrody <herdianafrody@JTA-INFRASs-MacBook-Pro-3.local>
2025-04-28 09:36:59 +0700

    Refactor: Implement Next Route API for API and Implement json-server

---
+++ src/app/api/denominations/route.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+import axios from "axios";
+
+export async function GET() {
+  try {
+    const response = await axios.get("http://localhost:3001/denominations");
+
+    return NextResponse.json(response.data);
+  } catch (error) {
+    console.error("Error fetching items: ", error);
+    return NextResponse.json(
+      { error: "Failed to fetch items" },
+      { status: 500 }
+    );
+  }
+}
